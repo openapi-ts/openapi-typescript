ea10b976a78617de810afa1cc286d381b1800adb
Drew Powers <drew@pow.rs>
2019-01-22 16:23:20 -0700

    Test Travis

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,7 @@ module.exports = {
   rules: {
     'arrow-parens': 0,
     'comma-dangle': 0,
-    'function-paren-newline': 0, // let Prettier handle it
-    'implicit-arrow-linebreak': 0, // let Prettier handle it
     'import/no-extraneous-dependencies': 0,
-    'object-curly-newline': 0, // let Prettier handle it
     'no-underscore-dangle': 0,
     'react/jsx-filename-extension': 0,
     'react/destructuring-assignment': 0,
