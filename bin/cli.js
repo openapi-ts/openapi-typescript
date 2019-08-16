a43a9bf562607535140b27d5f99905defa559668
trenoncourt <thibaut.renoncourt@gmail.com>
2019-08-16 11:33:07 +0200

    feat: add capability to use url input

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,7 @@ const chalk = require('chalk');
 const { dirname, resolve } = require('path');
 const meow = require('meow');
 const yaml = require('js-yaml');
+const axios = require('axios');
 const { default: swaggerToTS } = require('../dist-node');
 
 const cli = meow(
@@ -56,68 +57,76 @@ Options
     },
   }
 );
+(async () => {
+  let spec = cli.input[0];
 
-let spec = cli.input[0];
-
-if (typeof cli.flags.namespace === 'string' && cli.flags.namespace.length > 0) {
-  console.error(chalk.red('--namespace option is deprecated. Please use --wrapper instead.'));
-  return;
-}
-
-if (cli.flags.export === true) {
-  console.error(chalk.red('--export option is deprecated. Please use --wrapper instead.'));
-  return;
-}
-
-// If input is a file, load it
-const pathname = resolve(process.cwd(), spec);
-if (existsSync(pathname)) {
-  spec = readFileSync(pathname, 'UTF-8');
-}
-
-// Attempt to parse YAML
-try {
-  if (/\.ya?ml$/i.test(spec) || spec[0] !== '{') {
-    spec = yaml.safeLoad(spec);
+  if (typeof cli.flags.namespace === 'string' && cli.flags.namespace.length > 0) {
+    console.error(chalk.red('--namespace option is deprecated. Please use --wrapper instead.'));
+    return;
   }
-} catch (e) {
-  console.error(
-    chalk.red(`❌ "${spec}" seems to be YAML, but it couldn’t be parsed.
-  ${e}`)
-  );
-}
 
-// Attempt to parse JSON
-try {
-  if (typeof spec === 'string') {
-    spec = JSON.parse(spec);
+  if (cli.flags.export === true) {
+    console.error(chalk.red('--export option is deprecated. Please use --wrapper instead.'));
+    return;
   }
-} catch (e) {
-  console.error(
-    chalk.red(`❌ Could not parse JSON for "${spec}." Is this a valid Swagger spec?
+
+  // input is an url
+  if (spec.startsWith('http')) {
+    let response = await axios.get(spec, { headers: { 'Content-Type': 'text/plain' } });
+    spec = response.data;
+  }
+  // If input is a file, load it
+  else {
+    const pathname = resolve(process.cwd(), spec);
+    if (existsSync(pathname)) {
+      spec = readFileSync(pathname, 'UTF-8');
+    }
+  }
+
+  // Attempt to parse YAML
+  try {
+    if (/\.ya?ml$/i.test(spec) || spec[0] !== '{') {
+      spec = yaml.safeLoad(spec);
+    }
+  } catch (e) {
+    console.error(
+      chalk.red(`❌ "${spec}" seems to be YAML, but it couldn’t be parsed.
   ${e}`)
-  );
-}
+    );
+  }
 
-if (cli.flags.nowrapper) {
-  cli.flags.wrapper = false;
-}
+  // Attempt to parse JSON
+  try {
+    if (typeof spec === 'string') {
+      spec = JSON.parse(spec);
+    }
+  } catch (e) {
+    console.error(
+      chalk.red(`❌ Could not parse JSON for "${spec}." Is this a valid Swagger spec?
+  ${e}`)
+    );
+  }
 
-const result = swaggerToTS(spec, cli.flags);
+  if (cli.flags.nowrapper) {
+    cli.flags.wrapper = false;
+  }
 
-// Write to file if specifying output
-if (cli.flags.output) {
-  const timeStart = process.hrtime();
-  const outputFile = resolve(process.cwd(), cli.flags.output);
-  const parent = dirname(outputFile);
-  mkdirpSync(parent);
-  writeFileSync(outputFile, result);
+  const result = swaggerToTS(spec, cli.flags);
 
-  const timeEnd = process.hrtime(timeStart);
-  const time = timeEnd[0] + Math.round(timeEnd[1] / 1e6);
-  console.log(chalk.green(`🚀 ${cli.input[0]} -> ${chalk.bold(cli.flags.output)} [${time}ms]`));
-  return;
-}
+  // Write to file if specifying output
+  if (cli.flags.output) {
+    const timeStart = process.hrtime();
+    const outputFile = resolve(process.cwd(), cli.flags.output);
+    const parent = dirname(outputFile);
+    mkdirpSync(parent);
+    writeFileSync(outputFile, result);
 
-// Otherwise, return result
-return result;
+    const timeEnd = process.hrtime(timeStart);
+    const time = timeEnd[0] + Math.round(timeEnd[1] / 1e6);
+    console.log(chalk.green(`🚀 ${cli.input[0]} -> ${chalk.bold(cli.flags.output)} [${time}ms]`));
+    return;
+  }
+
+  // Otherwise, return result
+  return result;
+})();
