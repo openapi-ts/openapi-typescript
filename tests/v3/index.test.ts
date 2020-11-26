1e4d5972c6e52891a77aa0a72dfb31a0481ecaa9
Drew Powers <1369770+drwpow@users.noreply.github.com>
2020-11-26 15:58:16 -0700

    Update remote HTTP test (#388)

--- tests/v3/index.test.ts
+++ tests/v3/index.test.ts
@@ -17,12 +17,24 @@ describe("cli", () => {
   ["github", "stripe", "manifold", "petstore"].forEach((file) => {
     it(`reads ${file} spec (v3) from file`, () => {
       execSync(`../../pkg/bin/cli.js specs/${file}.yaml -o generated/${file}.ts`, {
-        cwd: path.resolve(__dirname),
+        cwd: __dirname,
       });
-      expect(fs.readFileSync(path.resolve(__dirname, `expected/${file}.ts`), "utf8")).toBe(
-        fs.readFileSync(path.resolve(__dirname, `generated/${file}.ts`), "utf8")
-      );
+      const expected = fs.readFileSync(path.join(__dirname, "expected", `${file}.ts`), "utf8");
+      const generated = fs.readFileSync(path.join(__dirname, "generated", `${file}.ts`), "utf8");
+      expect(generated).toBe(expected);
     });
+  });
+
+  it("reads spec (v3) from remote resource", () => {
+    execSync(
+      "../../pkg/bin/cli.js https://raw.githubusercontent.com/drwpow/openapi-typescript/main/tests/v3/specs/manifold.yaml -o generated/http.ts",
+      {
+        cwd: __dirname,
+      }
+    );
+    const expected = fs.readFileSync(path.join(__dirname, "expected", "http.ts"), "utf8");
+    const generated = fs.readFileSync(path.join(__dirname, "generated", "http.ts"), "utf8");
+    expect(generated).toBe(expected);
   });
 });
 
