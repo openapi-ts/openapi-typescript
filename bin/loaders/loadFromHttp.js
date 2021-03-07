3a20869c276d62098626dac256fe6082a57e2b5b
Drew Powers <1369770+drwpow@users.noreply.github.com>
2021-03-06 23:21:40 -0700

    Follow 3xx redirects (#510)
    Also fix an unreleased contentType bug from #508

--- bin/loaders/loadFromHttp.js
+++ bin/loaders/loadFromHttp.js
@@ -1,39 +1,54 @@
-const url = require("url");
+const http = require("http");
+const https = require("https");
+const { parse } = require("url");
 
-function loadFromHttp(pathToSpec, { auth }) {
-  const { protocol } = url.parse(pathToSpec);
+// config
+const MAX_REDIRECT_COUNT = 10;
 
-  if (protocol !== "http:" && protocol !== "https:") {
-    throw new Error(`Unsupported protocol: "${protocol}". URL must start with "http://" or "https://".`);
-  }
-
-  const fetch = require(protocol === "https:" ? "https" : "http");
+function fetch(url, opts, { redirectCount = 0 } = {}) {
   return new Promise((resolve, reject) => {
-    const req = fetch.request(
-      pathToSpec,
-      {
-        method: "GET",
-        auth,
-      },
-      (res) => {
-        let rawData = "";
-        res.setEncoding("utf8");
-        res.on("data", (chunk) => {
-          rawData += chunk;
-        });
-        res.on("end", () => {
-          if (res.statusCode >= 200 && res.statusCode < 300) {
-            resolve(rawData);
-          } else {
-            reject(rawData || `${res.statusCode} ${res.statusMessage}`);
+    const { protocol } = parse(url);
+
+    if (protocol !== "http:" && protocol !== "https:") {
+      throw new Error(`Unsupported protocol: "${protocol}". URL must start with "http://" or "https://".`);
+    }
+
+    const fetchMethod = protocol === "https:" ? https : http;
+    const req = fetchMethod.request(url, opts, (res) => {
+      let rawData = "";
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        rawData += chunk;
+      });
+      res.on("end", () => {
+        // 2xx: OK
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          return resolve(rawData);
+        }
+
+        // 3xx: follow redirect (if given)
+        if (res.statusCode >= 300 && res.headers.location) {
+          redirectCount += 1;
+          if (redirectCount >= MAX_REDIRECT_COUNT) {
+            reject(`Max redirects exceeded`);
+            return;
           }
-        });
-      }
-    );
+          console.log(`🚥 Redirecting to ${res.headers.location}…`);
+          return fetch(res.headers.location, opts).then(resolve);
+        }
+
+        // everything else: throw
+        return reject(rawData || `${res.statusCode} ${res.statusMessage}`);
+      });
+    });
     req.on("error", (err) => {
       reject(err);
     });
     req.end();
   });
+}
+
+function loadFromHttp(pathToSpec, { auth }) {
+  return fetch(pathToSpec, { method: "GET", auth });
 }
 module.exports = loadFromHttp;
