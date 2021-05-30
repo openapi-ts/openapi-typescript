9c2186448ce3c3c1c521da6d2d8ac15cf8bc1fc1
Rusty Conover <rusty@conover.me>
2021-05-30 10:10:39 -0400

    Fix handling of headers that are specified via reference.
    This commit makes headers by included in the produced types
    if they are included by reference.

--- src/transform/headers.ts
+++ src/transform/headers.ts
@@ -9,14 +9,24 @@ export function transformHeaderObjMap(
   let output = "";
 
   Object.entries(headerMap).forEach(([k, v]) => {
-    if (!v.schema) return;
-
     if (v.description) output += comment(v.description);
 
     const readonly = tsReadonly(options.immutableTypes);
+
+    if ("$ref" in v) {
+      const resolved = resolveRefIfNeeded<HeaderObject>(options.document, v);
+      if (resolved == null) {
+        throw new Error("Failed to resolve header ref");
+      }
+      v = resolved;
+    }
+    if (v.schema == null) {
+      return;
+    }
+
     const schema = resolveRefIfNeeded<SchemaObject>(options.document, v.schema);
 
-    const required = v.required || (schema != null && schema.default != null) ? "" : "?";
+    const required = (v.required || (schema != null && schema.default != null)) ? "" : "?";
 
     output += `  ${readonly}"${k}"${required}: ${transformSchemaObj(v.schema, options)}\n`;
   });
