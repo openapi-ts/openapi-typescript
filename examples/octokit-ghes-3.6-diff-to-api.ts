9ca3bb5d2d559207a39ffe9c9236d985150ff9eb
Loïc Fürhoff <loic.furhoff@pix4d.com>
2022-11-15 11:56:59 +0100

    Add type helpers only when used

--- examples/octokit-ghes-3.6-diff-to-api.ts
+++ examples/octokit-ghes-3.6-diff-to-api.ts
@@ -4,11 +4,6 @@
  */
 
 
-/** Type helpers */
-type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
-type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
-type OneOf<T extends any[]> = T extends [infer Only] ? Only : T extends [infer A, infer B, ...infer Rest] ? OneOf<[XOR<A, B>, ...Rest]> : never;
-
 export interface paths {
   "/admin/hooks": {
     /** List global webhooks */
