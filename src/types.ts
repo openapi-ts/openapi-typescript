8b34a06fb079a180adff905473797749c1c74416
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-04-26 13:18:11 -0600

    Fix missing parameters in operation object (#1090)

--- src/types.ts
+++ src/types.ts
@@ -276,7 +276,7 @@ export interface RequestBodyObject extends Extensable {
  */
 export interface MediaTypeObject extends Extensable {
   /** The schema defining the content of the request, response, or parameter. */
-  schema?: SchemaObject;
+  schema?: SchemaObject | ReferenceObject;
   /** Example of the media type. The example object SHOULD be in the correct format as specified by the media type. The example field is mutually exclusive of the examples field. Furthermore, if referencing a schema which contains an example, the example value SHALL override the example provided by the schema. */
   example?: any;
   /** Examples of the media type. Each example object SHOULD match the media type and specified schema if present. The examples field is mutually exclusive of the example field. Furthermore, if referencing a schema which contains an example, the examples value SHALL override the example provided by the schema. */
