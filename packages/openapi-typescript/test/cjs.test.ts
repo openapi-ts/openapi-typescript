1bf2d4db73b93fd1d36ffb56bdfb90321b0bfaba
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-07-31 12:33:13 -0600

    Add CJS package (#1263)

---
+++ packages/openapi-typescript/test/cjs.test.ts
@@ -0,0 +1,11 @@
+const fs = require('node:fs');
+const openapiTS = require('../dist/index.cjs');
+const yaml = require('js-yaml');
+
+describe('CJS bundle', () => {
+  it('basic',  async () => {
+    const input = yaml.load(fs.readFileSync(new URL('../examples/stripe-api.yaml', import.meta.url), 'utf8'));
+    const output = await openapiTS(input);
+    expect(output).toBe(fs.readFileSync(new URL('../examples/stripe-api.ts', import.meta.url), 'utf8'));
+  })
+})
