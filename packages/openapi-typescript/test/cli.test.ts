f196ac2d9f741a9f78fe8a4f854f26182547dd56
Drew Powers <drew@pow.rs>
2023-08-10 11:43:02 -0600

    Add Windows tests (back), skip DO test in macOS/Win

--- packages/openapi-typescript/test/cli.test.ts
+++ packages/openapi-typescript/test/cli.test.ts
@@ -1,8 +1,10 @@
 import { execa } from "execa";
-import fs from "node:fs";
-import { URL } from "node:url";
+import { URL, fileURLToPath } from "node:url";
+import os from "node:os";
+import { readFile } from "./helpers.js";
 
-const cwd = new URL("../", import.meta.url);
+const root = new URL("../", import.meta.url);
+const cwd = os.platform() === "win32" ? fileURLToPath(root) : root; // execa bug: fileURLToPath required on Windows
 const cmd = "./bin/cli.js";
 const TIMEOUT = 90000;
 
@@ -12,59 +14,60 @@ describe("CLI", () => {
     test(
       "GitHub API",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/github-api.ts", cwd), "utf8").trim();
+        const expected = readFile(new URL("./examples/github-api.ts", root)).trim();
         const { stdout } = await execa(cmd, ["./examples/github-api.yaml"], { cwd });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
     test(
       "GitHub API (next)",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/github-api-next.ts", cwd), "utf8").trim();
+        const expected = readFile(new URL("./examples/github-api-next.ts", root)).trim();
         const { stdout } = await execa(cmd, ["./examples/github-api-next.yaml"], { cwd });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
     test(
       "Octokit GHES 3.6 Diff to API",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/octokit-ghes-3.6-diff-to-api.ts", cwd), "utf8").trim();
+        const expected = readFile(new URL("./examples/octokit-ghes-3.6-diff-to-api.ts", root)).trim();
         const { stdout } = await execa(cmd, ["./examples/octokit-ghes-3.6-diff-to-api.json"], { cwd });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
     test(
       "Stripe API",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/stripe-api.ts", cwd), "utf8").trim();
+        const expected = readFile(new URL("./examples/stripe-api.ts", root)).trim();
         const { stdout } = await execa(cmd, ["./examples/stripe-api.yaml"], { cwd });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
-    test(
+    // this test runs too slowly on macos / windows in GitHub Actions (but not natively)
+    test.skipIf(process.env.CI_ENV === "macos" || process.env.CI_ENV === "windows")(
       "DigitalOcean API (remote $refs)",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/digital-ocean-api.ts", cwd), "utf8").trim();
+        const expected = readFile(new URL("./examples/digital-ocean-api.ts", root)).trim();
         const { stdout } = await execa(cmd, ["./examples/digital-ocean-api/DigitalOcean-public.v2.yaml"], {
           cwd,
         });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
     test(
       "stdin",
       async () => {
-        const expected = fs.readFileSync(new URL("./examples/stripe-api.ts", cwd), "utf8").trim();
-        const input = fs.readFileSync(new URL("./examples/stripe-api.yaml", cwd));
+        const expected = readFile(new URL("./examples/stripe-api.ts", root)).trim();
+        const input = readFile(new URL("./examples/stripe-api.yaml", root));
         const { stdout } = await execa(cmd, { input });
         expect(stdout).toBe(expected);
       },
-      TIMEOUT
+      TIMEOUT,
     );
   });
 
