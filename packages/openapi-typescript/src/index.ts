675ddb3aefe116d968457ed6f31cbd0d6cb9501a
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2023-08-25 17:38:22 +0200

    add line break

--- packages/openapi-typescript/src/index.ts
+++ packages/openapi-typescript/src/index.ts
@@ -126,7 +126,7 @@ async function openapiTS(schema: string | URL | OpenAPI3 | Readable, options: Op
 
   if (options.rootTypes) {
     for (const schema of Object.keys(typedComponents.schemas as object)) {
-      output.push(`export type ${schema} = components["schema"]["${schema}"];`);
+      output.push(`export type ${schema} = components["schema"]["${schema}"];\n`);
     }
   }
 
