eebe6a681d4af370d80f0c7a68c4cc763d3829f3
Drew Powers <drew@pow.rs>
2023-08-30 10:53:10 -0600

    Use Vitest .toMatchFileSnapshot()

--- packages/openapi-typescript/test/cjs.test.ts
+++ packages/openapi-typescript/test/cjs.test.ts
@@ -2,17 +2,14 @@
 
 // important: MUST use require()!
 const fs = require("node:fs");
-const { URL } = require("node:url");
+const { fileURLToPath, URL } = require("node:url");
 const openapiTS = require("../dist/index.cjs");
 const yaml = require("js-yaml");
-
-// note: this import is fine; it’s just a test helper
-import { readFile } from "./helpers.js";
 
 describe("CJS bundle", () => {
   it("basic", async () => {
     const input = yaml.load(fs.readFileSync(new URL("../examples/stripe-api.yaml", import.meta.url), "utf8"));
     const output = await openapiTS(input);
-    expect(output).toBe(readFile(new URL("../examples/stripe-api.ts", import.meta.url)));
+    expect(output).toMatchFileSnapshot(fileURLToPath(new URL("../examples/stripe-api.ts", import.meta.url)));
   });
 });
