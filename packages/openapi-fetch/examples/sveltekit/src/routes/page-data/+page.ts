e76e7150ea3b599de860734e98180327eba2f92d
hugeletters <ckuka71@gmail.com>
2023-10-07 18:34:12 +0200

    updated example

--- packages/openapi-fetch/examples/sveltekit/src/routes/page-data/+page.ts
+++ packages/openapi-fetch/examples/sveltekit/src/routes/page-data/+page.ts
@@ -1,14 +1,12 @@
-import { createServerClient } from "$lib/api/index.js";
+import client from "$lib/api/index.js";
 
 // Note: this uses Svelte’s custom fetcher as an example, but Node’s
 // native fetch works, too. See Svelte’s docs to learn the difference:
 // @see https://kit.svelte.dev/docs/load#making-fetch-requests
 export async function load({ fetch }) {
-  const client = createServerClient(fetch);
   const fact = await client.GET("/fact", {
-    params: {
-      query: { max_length: 500 },
-    },
+    params: { query: { max_length: 500 } },
+    fetch,
   });
 
   return {
