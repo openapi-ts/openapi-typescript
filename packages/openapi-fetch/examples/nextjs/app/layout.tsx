ee87e2178a39a1a679b51c5ebabda9888065fb2a
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-11-03 09:37:15 -0600

    Update openapi-fetch NextJS example (#1427)

--- packages/openapi-fetch/examples/nextjs/app/layout.tsx
+++ packages/openapi-fetch/examples/nextjs/app/layout.tsx
@@ -1,10 +1,15 @@
+import React from "react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "openapi-fetch + Next.js",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
