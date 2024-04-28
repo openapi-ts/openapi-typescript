f60c0a73985a9ed8b02cbe9ff43b087a2c2f865e
Drew Powers <drew@pow.rs>
2024-04-28 15:59:59 -0600

    Add additional examples to script

--- packages/openapi-typescript/scripts/update-examples.ts
+++ packages/openapi-typescript/scripts/update-examples.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 import { multiFile, singleFile } from "./schemas.js";
 
 async function generateSchemas() {
-  const cwd = new URL("../", import.meta.url);
+  const rootCWD = new URL("../", import.meta.url);
   const singleFileSchemas = Object.entries(singleFile);
   const multiFileSchemas = Object.entries(multiFile);
 
@@ -12,17 +12,38 @@ async function generateSchemas() {
 
   const updateSchema = async (name: string, ext: string) => {
     const start = performance.now();
+    const cwd =
+      process.platform === "win32"
+        ? // execa/cross-spawn can not handle URL objects on Windows, so convert it to string and cut away the protocol
+          rootCWD
+            .toString()
+            .slice("file:///".length)
+        : rootCWD;
 
     try {
-      await execa("./bin/cli.js", [`./examples/${name}${ext}`, "-o", `./examples/${name}.ts`], {
-        cwd:
-          process.platform === "win32"
-            ? // execa/cross-spawn can not handle URL objects on Windows, so convert it to string and cut away the protocol
-              cwd
-                .toString()
-                .slice("file:///".length)
-            : cwd,
-      });
+      await Promise.all([
+        execa("./bin/cli.js", [`./examples/${name}${ext}`, "-o", `./examples/${name}.ts`], { cwd }),
+        ...(name === "github-api"
+          ? [
+              execa(
+                "./bin/cli.js",
+                [
+                  `./examples/${name}${ext}`,
+                  "--immutable",
+                  "--export-type",
+                  "-o",
+                  `./examples/${name}-export-type-immutable.ts`,
+                ],
+                { cwd },
+              ),
+              execa(
+                "./bin/cli.js",
+                [`./examples/${name}${ext}`, "--immutable", "-o", `./examples/${name}-immutable.ts`],
+                { cwd },
+              ),
+            ]
+          : []),
+      ]);
 
       schemasDoneCount++;
       const timeMs = Math.round(performance.now() - start);
