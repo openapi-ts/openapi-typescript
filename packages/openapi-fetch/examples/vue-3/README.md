aae40e890fe403130081b79e7b8874fb98edcfc0
hungify <nmhungify@gmail.com>
2024-05-04 21:41:50 +0700

    docs: Add Vue 3 example for openapi-fetch

---
+++ packages/openapi-fetch/examples/vue-3/README.md
@@ -0,0 +1,12 @@
+# openapi-fetch + Vue
+
+Example of using openapi-fetch with [Vue](https://vuejs.org/).
+
+## Setup
+
+```sh
+pnpm i
+pnpm run dev
+```
+
+You’ll see the server running at `localhost:5173`
---
+++ packages/openapi-fetch/examples/vue-3/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useCatFactQuery } from "./composables/catfact-query";
+
+const { error, isFetching, isReady, state, execute } = useCatFactQuery({
+  params: {
+    query: {
+      max_length: 140,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="demo">
+    <button @click="execute">Execute</button>
+    <pre class="code-block">{{
+      JSON.stringify(
+        {
+          isReady,
+          isFetching,
+          error,
+          state,
+        },
+        undefined,
+        2
+      )
+    }}</pre>
+  </div>
+</template>
+
+<style scoped>
+.demo {
+  padding: 16px;
+  margin: 0 auto;
+  width: 800px;
+}
+.demo .code-block {
+  background-color: #202127;
+  padding: 4px 8px;
+  margin: 12px 0;
+  border-radius: 4px;
+  word-break: break-all;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+</style>
