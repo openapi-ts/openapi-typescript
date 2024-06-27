429b2b0bffc622cab43ce34cfcd91fb34e7f7d14
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-06-27 14:19:41 -0300

    style: Update conditional check for FormData instance

--- packages/openapi-fetch/src/index.js
+++ packages/openapi-fetch/src/index.js
@@ -78,10 +78,10 @@ export default function createClient(clientOptions) {
     };
     if (requestInit.body) {
       requestInit.body = bodySerializer(requestInit.body);
-    }
-    // remove `Content-Type` if serialized body is FormData; browser will correctly set Content-Type & boundary expression
-    if (requestInit.body && requestInit.body instanceof FormData) {
-      requestInit.headers.delete("Content-Type");
+      // remove `Content-Type` if serialized body is FormData; browser will correctly set Content-Type & boundary expression
+      if (requestInit.body instanceof FormData) {
+        requestInit.headers.delete("Content-Type");
+      }
     }
     let request = new CustomRequest(createFinalURL(url, { baseUrl, params, querySerializer }), requestInit);
     // middleware (request)
