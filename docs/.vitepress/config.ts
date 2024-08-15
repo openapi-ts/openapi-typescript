11ed757ace89162e9019d067ca0077a681665de2
Hikaru Yoshino <osushicrusher@gmail.com>
2024-08-15 20:40:40 +0900

    feat: add vitepress `ja` config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vitepress";
 import en from "./en";
 import zh from "./zh";
+import ja from "./ja";
 import shared from "./shared";
 
 // https://vitepress.dev/reference/site-config
@@ -9,5 +10,6 @@ export default defineConfig({
   locales: {
     root: { label: "English", ...en },
     zh: { label: "简体中文", ...zh },
+    ja: { label: "日本語", ...ja },
   },
 });
