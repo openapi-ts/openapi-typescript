080673d171143c389f75cf24325037d84aa6198e
djordy <djordy.koert@yoursurprise.com>
2024-10-25 19:22:59 +0200

    better @ts-expect-error scoping

--- packages/openapi-fetch/test/types.test.ts
+++ packages/openapi-fetch/test/types.test.ts
@@ -20,93 +20,146 @@ describe("types", () => {
       test("returns all possible responses", () => {
         type Response = GetResponseContent<MixedResponses>;
         assertType<Response>({ data: "200 application/json" });
-        // @ts-expect-error It picks literal over string
-        assertType<Response>({ data: "200 but different string" });
+        assertType<Response>({
+          // @ts-expect-error It picks literal over string
+          data: "200 but different string",
+        });
         assertType<Response>("200 text/plain");
         assertType<Response>("206 text/plain");
         assertType<Response>("404 text/plain");
         assertType<Response>({ error: "500 application/json" });
-        // @ts-expect-error 204 never does not become undefined
-        assertType<Response>(undefined);
+        assertType<Response>(
+          // @ts-expect-error 204 never does not become undefined
+          undefined,
+        );
       });
 
       test("returns correct type for 200 with literal", () => {
         type Response = GetResponseContent<MixedResponses, `${string}/${string}`, 200>;
         assertType<Response>({ data: "200 application/json" });
         assertType<Response>("200 text/plain");
-        // @ts-expect-error
-        assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
-        // @ts-expect-error
-        assertType<Response>({ error: "500 application/json" });
+        assertType<Response>(
+          // @ts-expect-error
+          "206 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
+        assertType<Response>({
+          // @ts-expect-error
+          error: "500 application/json",
+        });
       });
 
       test("returns correct type for 200 with json-like literal", () => {
         type Response = GetResponseContent<MixedResponses, `${string}/json`, 200>;
         assertType<Response>({ data: "200 application/json" });
-        // @ts-expect-error
-        assertType<Response>("200 text/plain");
-        // @ts-expect-error
-        assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
-        // @ts-expect-error
-        assertType<Response>({ error: "500 application/json" });
+        assertType<Response>(
+          // @ts-expect-error
+          "200 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "206 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
+        assertType<Response>({
+          // @ts-expect-error
+          error: "500 application/json",
+        });
       });
 
       test("returns correct type for 200 with application/json", () => {
         type Response = GetResponseContent<MixedResponses, "application/json", 200>;
         assertType<Response>({ data: "200 application/json" });
-        // @ts-expect-error
-        assertType<Response>("200 text/plain");
-        // @ts-expect-error
-        assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
-        // @ts-expect-error
-        assertType<Response>({ error: "500 application/json" });
+        assertType<Response>(
+          // @ts-expect-error
+          "200 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "206 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
+        assertType<Response>({
+          // @ts-expect-error
+          error: "500 application/json",
+        });
       });
 
       test("returns 200 & 500 responses", () => {
         type Response = GetResponseContent<MixedResponses, `${string}/${string}`, 200 | 500>;
         assertType<Response>({ data: "200 application/json" });
         assertType<Response>("200 text/plain");
-        // @ts-expect-error
-        assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
+        assertType<Response>(
+          // @ts-expect-error
+          "206 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
         assertType<Response>({ error: "500 application/json" });
       });
 
       test("returns all OK responses", () => {
-        // @ts-expect-error: Type 'OkStatus' does not satisfy the constraint 'keyof MixedResponses'. Can safely ignore this error.
-        type Response = GetResponseContent<MixedResponses, `${string}/${string}`, OkStatus>;
+        type Response = GetResponseContent<
+          MixedResponses,
+          `${string}/${string}`,
+          // @ts-expect-error: Type 'OkStatus' does not satisfy the constraint 'keyof MixedResponses'. Can safely ignore this error.
+          OkStatus
+        >;
         assertType<Response>({ data: "200 application/json" });
         assertType<Response>("200 text/plain");
         assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
-        // @ts-expect-error
-        assertType<Response>({ error: "500 application/json" });
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
+        assertType<Response>({
+          // @ts-expect-error
+          error: "500 application/json",
+        });
       });
 
       test("non existent media type", () => {
         type Response = GetResponseContent<MixedResponses, "I/DO NOT EXIST">;
-        // @ts-expect-error
-        assertType<Response>({ data: "200 application/json" });
-        // @ts-expect-error
-        assertType<Response>({ data: "200 but different string" });
-        // @ts-expect-error
-        assertType<Response>("200 text/plain");
-        // @ts-expect-error
-        assertType<Response>("206 text/plain");
-        // @ts-expect-error
-        assertType<Response>("404 text/plain");
-        // @ts-expect-error
-        assertType<Response>({ error: "500 application/json" });
-        // @ts-expect-error 204 never does not become undefined
-        assertType<Response>(undefined);
+        assertType<Response>({
+          // @ts-expect-error
+          data: "200 application/json",
+        });
+        assertType<Response>({
+          // @ts-expect-error
+          data: "200 but different string",
+        });
+        assertType<Response>(
+          // @ts-expect-error
+          "200 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "206 text/plain",
+        );
+        assertType<Response>(
+          // @ts-expect-error
+          "404 text/plain",
+        );
+
+        assertType<Response>({
+          // @ts-expect-error
+          error: "500 application/json",
+        });
+        assertType<Response>(
+          // @ts-expect-error 204 never does not become undefined
+          undefined,
+        );
       });
     });
 
@@ -141,21 +194,31 @@ describe("types", () => {
       assertType<Response>({ data: "200 application/json" });
       assertType<Response>("200 text/plain");
       assertType<Response>("206 text/plain");
-      // @ts-expect-error
-      assertType<Response>("404 text/plain");
-      // @ts-expect-error
-      assertType<Response>({ error: "500 application/json" });
+      assertType<Response>(
+        // @ts-expect-error
+        "404 text/plain",
+      );
+      assertType<Response>({
+        // @ts-expect-error
+        error: "500 application/json",
+      });
     });
 
     test("returns all 2XX responses, only application/json", () => {
       type Response = SuccessResponse<Responses, "application/json">;
       assertType<Response>({ data: "200 application/json" });
-      // @ts-expect-error
-      assertType<Response>("200 text/plain");
-      // @ts-expect-error
-      assertType<Response>("206 text/plain");
-      // @ts-expect-error
-      assertType<Response>("404 text/plain");
+      assertType<Response>(
+        // @ts-expect-error
+        "200 text/plain",
+      );
+      assertType<Response>(
+        // @ts-expect-error
+        "206 text/plain",
+      );
+      assertType<Response>(
+        // @ts-expect-error
+        "404 text/plain",
+      );
       // @ts-expect-error
       assertType<Response>({ error: "500 application/json" });
     });
@@ -178,12 +241,18 @@ describe("types", () => {
 
     test("returns all 5XX and 4xx responses", () => {
       type Response = ErrorResponse<Responses>;
-      // @ts-expect-error
-      assertType<Response>({ data: "200 application/json" });
-      // @ts-expect-error
-      assertType<Response>("200 text/plain");
-      // @ts-expect-error
-      assertType<Response>("206 text/plain");
+      assertType<Response>({
+        // @ts-expect-error
+        data: "200 application/json",
+      });
+      assertType<Response>(
+        // @ts-expect-error
+        "200 text/plain",
+      );
+      assertType<Response>(
+        // @ts-expect-error
+        "206 text/plain",
+      );
       assertType<Response>("404 text/plain");
       assertType<Response>({ error: "500 application/json" });
       assertType<Response>({ error: "default application/json" });
@@ -191,14 +260,22 @@ describe("types", () => {
 
     test("returns all 5XX and 4xx responses, only application/json", () => {
       type Response = ErrorResponse<Responses, "application/json">;
-      // @ts-expect-error
-      assertType<Response>({ data: "200 application/json" });
-      // @ts-expect-error
-      assertType<Response>("200 text/plain");
-      // @ts-expect-error
-      assertType<Response>("206 text/plain");
-      // @ts-expect-error
-      assertType<Response>("404 text/plain");
+      assertType<Response>({
+        // @ts-expect-error
+        data: "200 application/json",
+      });
+      assertType<Response>(
+        // @ts-expect-error
+        "200 text/plain",
+      );
+      assertType<Response>(
+        // @ts-expect-error
+        "206 text/plain",
+      );
+      assertType<Response>(
+        // @ts-expect-error
+        "404 text/plain",
+      );
       assertType<Response>({ error: "500 application/json" });
       assertType<Response>({ error: "default application/json" });
     });
