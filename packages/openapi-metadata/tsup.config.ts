9b4df61bb6cda0f9e6eb402e7211918c611aa473
Martin PAUCOT <contact@martin-paucot.fr>
2024-11-06 19:23:48 +0100

    style(openapi-metadata): lint

--- packages/openapi-metadata/tsup.config.ts
+++ packages/openapi-metadata/tsup.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/decorators/index.ts",
-    "src/errors/index.ts",
-    "src/metadata/index.ts",
-    "src/ui/index.ts",
-  ],
+  entry: ["src/index.ts", "src/decorators/index.ts", "src/errors/index.ts", "src/metadata/index.ts", "src/ui/index.ts"],
   format: ["esm"],
   dts: true,
   sourcemap: true,
