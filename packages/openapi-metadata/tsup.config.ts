6126497617e65b688d091f3772a352b6b4ff13a6
Martin PAUCOT <contact@martin-paucot.fr>
2025-01-23 15:13:01 +0100

    chore(metadata): migrate to unbuild

--- packages/openapi-metadata/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["src/index.ts", "src/decorators/index.ts", "src/errors/index.ts", "src/metadata/index.ts", "src/ui/index.ts"],
-  format: ["esm"],
-  dts: true,
-  sourcemap: true,
-});
