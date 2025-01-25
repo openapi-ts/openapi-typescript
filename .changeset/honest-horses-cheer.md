0a01774c8b3a091545ae5c1e615adc9fabb9d475
Martin Paucot <contact@martin-paucot.fr>
2025-01-25 21:02:11 +0100

    chore(metadata): migrate to unbuild (#2115)
    Co-authored-by: Drew Powers <drew@pow.rs>

---
+++ .changeset/honest-horses-cheer.md
@@ -0,0 +1,5 @@
+---
+"openapi-metadata": minor
+---
+
+[#2097](https://github.com/openapi-ts/openapi-typescript/pull/2097): Migrate to unbuild and provide CJS support
---
+++ packages/openapi-metadata/src/errors/index.ts
@@ -0,0 +1,4 @@
+export { InvalidConfigurationError } from "./invalid-configuration.js";
+export { NoExplicitTypeError } from "./no-explicit-type.js";
+export { ReflectMetadataMissingError } from "./reflect-metadata-missing.js";
+export { SymbolKeysNotSupportedError } from "./symbol-keys-not-supported.js";
