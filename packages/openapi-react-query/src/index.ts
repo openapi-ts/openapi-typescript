bd874bd756e32678e761d49b7ea7836a8dbb3c64
patrickariel <161032380+patrickariel@users.noreply.github.com>
2025-01-28 18:15:16 +0700

    refactor(openapi-react-query): simplify types

--- packages/openapi-react-query/src/index.ts
+++ packages/openapi-react-query/src/index.ts
@@ -118,7 +118,7 @@ export type UseInfiniteQueryMethod<Paths extends Record<string, Record<HttpMetho
       QueryKey<Paths, Method, Path>,
       Query[PageParamName]
     >,
-    "queryKey" | "queryFn" | "initialPageParam"
+    "queryKey" | "queryFn"
   >,
 >(
   method: Method,
@@ -126,7 +126,6 @@ export type UseInfiniteQueryMethod<Paths extends Record<string, Record<HttpMetho
   init: InitWithUnknowns<Init>,
   options: Options & {
     pageParamName: PageParamName;
-    initialPageParam: Query[PageParamName];
   },
   queryClient?: QueryClient,
 ) => UseInfiniteQueryResult<InfiniteData<Response["data"]>, Response["error"]>;
