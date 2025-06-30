0819a13679d99d9d3adfb5e404dcd5b597a623c2
HasutoSasaki <hasuto.sa@gmail.com>
2025-07-01 06:58:05 +0900

    docs: Remove Nuxt 3 example files for openapi-fetch

--- packages/openapi-fetch/examples/nuxt-3/lib/api/index.ts
+++
@@ -1,6 +0,0 @@
-import type { paths } from "@/lib/api/v1";
-import createClient from "openapi-fetch";
-
-const client = createClient<paths>({ baseUrl: "https://catfact.ninja/" });
-
-export default client;
