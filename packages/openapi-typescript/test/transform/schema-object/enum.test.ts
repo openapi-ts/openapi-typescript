968b6934884ac6b9e4ce2258fe377c9c8b1d2e89
dan <dan@staykind.io>
2025-09-08 15:57:24 +1000

    add baseline test for disabled enums flag

--- packages/openapi-typescript/test/transform/schema-object/enum.test.ts
+++ packages/openapi-typescript/test/transform/schema-object/enum.test.ts
@@ -4,6 +4,77 @@ import { DEFAULT_CTX, type TestCase } from "../../test-helpers.js";
 
 const tests: TestCase[] = [
   [
+    "options > enum: false and conditionalEnums: false",
+    {
+      given: mockSchema(),
+      want: `export interface paths {
+    "/status": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Get current status */
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description Status response */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "application/json": {
+                            required?: unknown;
+                            status?: components["schemas"]["StatusResponse"];
+                            statusEnum?: components["schemas"]["StatusEnumResponse"];
+                        };
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+}
+export type webhooks = Record<string, never>;
+export interface components {
+    schemas: {
+        StatusResponse: {
+            status?: components["schemas"]["Status"];
+        };
+        /** @enum {string} */
+        Status: "pending" | "active" | "done";
+        StatusEnumResponse: {
+            status?: components["schemas"]["StatusEnum"];
+        };
+        /** @enum {string} */
+        StatusEnum: "pending" | "active" | "done";
+    };
+    responses: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
+}
+export type $defs = Record<string, never>;
+export type operations = Record<string, never>;`,
+      options: { ctx: createTestContext({ enum: false, conditionalEnums: false }) },
+    },
+  ],
+  [
     "options > enum: true and conditionalEnums: false",
     {
       given: mockSchema(),
