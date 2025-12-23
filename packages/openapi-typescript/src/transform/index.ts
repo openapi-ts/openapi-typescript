f2cf767905b2ca0d33178349586ea0547c3260dd
abumalick <abdullah.hilson@gmail.com>
2025-12-23 14:43:40 +0300

    fix: export read-write helper types in generated output

--- packages/openapi-typescript/src/transform/index.ts
+++ packages/openapi-typescript/src/transform/index.ts
@@ -20,10 +20,10 @@ const transformers: Record<SchemaTransforms, (node: any, options: GlobalContext)
 
 // Inline helper types for readOnly/writeOnly markers (when readWriteMarkers is enabled)
 const READ_WRITE_HELPER_TYPES = `
-type $Read<T> = { readonly $read: T };
-type $Write<T> = { readonly $write: T };
-type Readable<T> = T extends $Write<any> ? never : T extends $Read<infer U> ? U : T extends (infer E)[] ? Readable<E>[] : T extends object ? { [K in keyof T as NonNullable<T[K]> extends $Write<any> ? never : K]: Readable<T[K]> } : T;
-type Writable<T> = T extends $Read<any> ? never : T extends $Write<infer U> ? U : T extends (infer E)[] ? Writable<E>[] : T extends object ? { [K in keyof T as NonNullable<T[K]> extends $Read<any> ? never : K]: Writable<T[K]> } & { [K in keyof T as NonNullable<T[K]> extends $Read<any> ? K : never]?: never } : T;
+export type $Read<T> = { readonly $read: T };
+export type $Write<T> = { readonly $write: T };
+export type Readable<T> = T extends $Write<any> ? never : T extends $Read<infer U> ? U : T extends (infer E)[] ? Readable<E>[] : T extends object ? { [K in keyof T as NonNullable<T[K]> extends $Write<any> ? never : K]: Readable<T[K]> } : T;
+export type Writable<T> = T extends $Read<any> ? never : T extends $Write<infer U> ? U : T extends (infer E)[] ? Writable<E>[] : T extends object ? { [K in keyof T as NonNullable<T[K]> extends $Read<any> ? never : K]: Writable<T[K]> } & { [K in keyof T as NonNullable<T[K]> extends $Read<any> ? K : never]?: never } : T;
 `;
 
 export default function transformSchema(schema: OpenAPI3, ctx: GlobalContext) {
